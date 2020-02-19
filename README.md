8a670a0617f0c6a38ce183c890025a594621adcf
Stephan Traub <stetrau@aol.com>
2020-02-20 00:10:51 +0100

    update the functions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wiz_light
 A WiZ Light intergration for Home Assistant
+
+## Install for testing 
+If you want to try the integration please clone this repo to `<confdir>/custom_components/`
+You also have to install the `pip install pywizlight` package.
+
--- light.py
+++ light.py
@@ -3,10 +3,12 @@ import logging
 import voluptuous as vol
 from pywizlight import wizlight
 
+
+import homeassistant.util.color as color_utils
 import homeassistant.helpers.config_validation as cv
 # Import the device class from the component that you want to support
 from homeassistant.components.light import (
-    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, Light)
+    ATTR_BRIGHTNESS, PLATFORM_SCHEMA, Light, ATTR_RGB_COLOR, SUPPORT_COLOR, SUPPORT_BRIGHTNESS)
 from homeassistant.const import CONF_HOST
 
 _LOGGER = logging.getLogger(__name__)
@@ -16,6 +18,7 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_HOST): cv.string,
 })
 
+SUPPORT_FEATURES = (SUPPORT_BRIGHTNESS | SUPPORT_COLOR )
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the WiZ Light platform."""
@@ -36,11 +39,23 @@ class WizBulb(Light):
         self._light = light
         self._state = None
         self._brightness = None
+        self._name = None
+        self._rgb = None
 
     @property
     def brightness(self):
         """Return the brightness of the light. """
         return self._brightness
+
+    @property
+    def rgb_color(self):
+        """Return the color property."""
+        return self._color
+
+    @property
+    def name(self):
+        """Return the ip as name of the device if any."""
+        return self._light.ip
 
     @property
     def is_on(self):
@@ -49,12 +64,20 @@ class WizBulb(Light):
 
     def turn_on(self, **kwargs):
         """Instruct the light to turn on."""
-        self._light.brightness = self.hex_to_percent(kwargs.get(ATTR_BRIGHTNESS, 255))
+        if ATTR_RGB_COLOR in kwargs:
+            self._rgb = kwargs[ATTR_RGB_COLOR]
+        if ATTR_BRIGHTNESS in kwargs:
+            self._brightness = kwargs[ATTR_BRIGHTNESS]
         self._light.turn_on()
 
     def turn_off(self, **kwargs):
         """Instruct the light to turn off."""
         self._light.turn_off()
+
+    @property
+    def supported_features(self) -> int:
+        """Flag supported features."""
+        return SUPPORT_FEATURES
 
     def update(self):
         """Fetch new state data for this light.
@@ -63,6 +86,9 @@ class WizBulb(Light):
         """
         self._state = self._light.status
         self._brightness = self._light.brightness
+        self._color = self._light.color
+        self._name = self._light.ip
+        self._rgb = self._light.rgb
 
     def hex_to_percent(self, hex):
         return (hex/255)*100
