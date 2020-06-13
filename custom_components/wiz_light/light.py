47cd699a759794d25fd029cc713144b8fa39237e
Stephan Traub <stetrau@aol.com>
2020-06-13 18:59:22 +0200

    Remove dupl. bulb def.

--- custom_components/wiz_light/light.py
+++ custom_components/wiz_light/light.py
@@ -153,7 +153,7 @@ class WizBulb(LightEntity):
         if self._bulbtype == "ESP06_SHDW9_01":
             return SUPPORT_BRIGHTNESS | SUPPORT_EFFECT
         # Color Temp and dimmer - not tested
-        if self._bulbtype == "ESP01_SHTW1C_31" or self._bulbtype == "ESP15_SHTW1_01I":
+        if self._bulbtype == "ESP01_SHTW1C_31":
             return SUPPORT_BRIGHTNESS | SUPPORT_COLOR_TEMP
         # Firlament bulbs support only dimmer (tested)
         if self._bulbtype == "ESP56_SHTW3_01" or self._bulbtype == "ESP15_SHTW1_01I":
