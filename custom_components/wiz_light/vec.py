711a0105e4d8834fda783c0cc52a62d08ebe0b5f
Stephan Traub <stetrau@aol.com>
2021-01-28 19:51:11 +0100

    Resolve conflicts

--- custom_components/wiz_light/vec.py
+++ custom_components/wiz_light/vec.py
@@ -4,8 +4,8 @@
 class MathHelper:
     """Helper class with some math.
 
-<<<<<<< HEAD
-    A bunch of utility functions, just so we don't have to bring in any external dependencies.
+    <<<<<<< HEAD
+        A bunch of utility functions, just so we don't have to bring in any external dependencies.
     """
 
     # a small value, really close to zero, more than adequate for our 3 orders of magnitude
@@ -63,46 +63,3 @@ class MathHelper:
     def vecFromAngle(angle):
         """Retruns something."""
         return (cos(angle), sin(angle))
-=======
-# a bunch of utility functions, just so we don't have to bring in any external dependencies
-def vecDot(a, b):
-    return sum(map(operator.mul, a, b))
-
-
-def vecLenSq(a):
-    return vecDot(a, a)
-
-
-def vecLen(a):
-    lenSq = vecLenSq(a)
-    return math.sqrt(lenSq) if (lenSq > epsilon) else 0
-
-
-def vecAdd(a, b):
-    return tuple(map(operator.add, a, b))
-
-
-def vecSub(a, b):
-    return tuple(map(operator.sub, a, b))
-
-
-def vecMul(vec, sca):
-    return tuple([c * sca for c in vec])
-
-
-def vecInt(vec):
-    return tuple([int(c) for c in vec])
-
-
-def vecNormalize(vec):
-    len = vecLen(vec)
-    return vecMul(vec, 1 / len) if (len > epsilon) else vec
-
-
-def vecFormat(vec):
-    return "({})".format(str([float("{0:.3f}".format(n)) for n in vec])[1:-1])
-
-
-def vecFromAngle(angle):
-    return (math.cos(angle), math.sin(angle))
->>>>>>> 87a3ca8bc173c84da307a386e6cc99b641166e15
